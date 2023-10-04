de9345a4a136eddbc76624136633396388fbb7e3
Urien Nolasco <urien.dragon@gmail.com>
2023-10-04 15:25:07 -0300

    Colocando os metodos removePedido e fechaPedido

--- Empresa.java
+++ Empresa.java
@@ -24,12 +24,31 @@ public class Empresa {
 
 	}
 
-	public void removePedido() {
 
+	public void removePedido(int numPedido, int quantidade) {
+		if(aberta && numPedido >= 1 && numPedido <= listPedido.size() && quantidade > 0){
+			Pedido pedido = listPedido.get(numPedido - 1);
+			if(quantidade < pedido.getQuantidade()){
+				pedido.setQuantiade(pedido.getQuantiade() - quantidade);
+			} else{
+				listPedido.remove(pedido);
+			}
+		}
 	}
+	public void fechaPedido(Pedido pedido) {
 
-	public void fechaPedido() {
+		boolean pedidoEncontrado = false;
 
+		for(Pedido p : listaPedidos){
+			if(p.equals(pedido)){
+				pedidoEncontrado = true;
+				p.setStatus("Pedido Fechado");
+				getPedidosDescricao();
+			}
+		}
+		if(!pedidoEncontrado){
+			System.out.println("Pedido não encontrado. Não foi possível fechá-lo.");
+		}
 	}
 
 	public void getEstatisticas() {
