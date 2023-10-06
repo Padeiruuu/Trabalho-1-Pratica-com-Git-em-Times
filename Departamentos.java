9237514815f312a046f77f379f934259a5002ac7
Eduardo Wolf <eduardomartiniwolf@gmail.com>
2023-10-06 09:14:03 -0300

    Implementação dos métodos na classe Departamentos

--- Departamentos.java
+++ Departamentos.java
@@ -1,21 +1,31 @@
+import java.util.ArrayList;
 public class Departamentos {
 
-	private int valorMáximo;
+	private int valorMaximo;
+	private String nome;
+	private ArrayList<Funcionario> funcionarios;
 
-	public void getNome() {
-
+	public Departamentos(String nome, int valorMaximo){
+		this.valorMaximo=valorMaximo;
+		this.nome=nome;
+		this.funcionarios = new ArrayList<>();
+	}
+	public String getNome() {
+		return nome;
 	}
 
-	public void getFuncionarios() {
-
+	public ArrayList<Funcionario> getFuncionarios() {
+		return funcionarios;
 	}
 
-	public void addFuncionarios() {
-
+	public void addFuncionarios(Funcionario funcionario) {
+		funcionarios.add(funcionario);
+		System.out.println("Funcionário cadastrado no departamento "+getNome());
 	}
 
-	public void removeFuncionarios() {
-
+	public void removeFuncionarios(Funcionario funcionario) {
+		funcionarios.remove(funcionario);
+		System.out.println("Não foi possível cadastrar o funcionário no departamento "+getNome());
 	}
 
 }
