e61253cef0df4b7dba8bfc3ad9ea1f15e104ba8b
Enzo Zortea <ezorteavivian@gmail.com>
2023-10-06 15:00:51 -0300

    Classe admnistrador e lista de pedidos entre datas

--- Administrador.java
+++ Administrador.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class Administrador extends Usuário {
+    
+    private ArrayList<Pedido> pedidos;
+
+    public Administrador() {
+        super(int identificador, String nome);
+    }
+
+    public ArrayList pedidosEntreDatas(int dataInicial, int dataFinal) {
+        ArrayList<Pedido> pedidosEntreDatas = new ArrayList<Pedido>();
+        for(Pedido pedido : pedidos) {
+            if(pedido.getDataPedido() >= dataInicial && pedido.getDataPedido() <= dataFinal) {
+                pedidosEntreDatas.add(pedido);
+            }
+        }
+        return pedidosEntreDatas;
+    }
+
 
 }
