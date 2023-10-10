9f34dca1292159d635eab68bd0bda672d29c2e8b
Enzo Zortea <ezorteavivian@gmail.com>
2023-10-09 21:28:08 -0300

    Correção do metodo 'pedidosEntreDatas' e correção dos tipos e nomes de variaveis

--- Administrador.java
+++ Administrador.java
@@ -1,43 +1,59 @@
 import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Administrador extends Usuário {
+public class Administrador extends Usuario {
     
     private ArrayList<Pedido> pedidos;
 
-    public Administrador(int identificador, int nome) {
-        super(identificador, nome);
+    public Administrador(int identificador, String nome, String iniciais) {
+        super(identificador, nome, iniciais, true);
     }
 
-    public ArrayList<Pedido> pedidosEntreDatas(int dataInicial, int dataFinal) {
-        ArrayList<Pedido> pedidosEntreDatas = new ArrayList<Pedido>();
-        for(Pedido pedido : pedidos) {
-            if(pedido.getDataPedido() >= dataInicial && pedido.getDataPedido() <= dataFinal) {
-                pedidosEntreDatas.add(pedido);
+    public ArrayList<Pedido> pedidosEntreDatas(String dataInicial, String dataFinal) {
+        ArrayList<Pedido> pedidosEntreDatas = new ArrayList<>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    
+        try {
+            Date inicio = dateFormat.parse(dataInicial);
+            Date fim = dateFormat.parse(dataFinal);
+    
+            for (Pedido pedido : pedidos) {
+                Date dataPedido = dateFormat.parse(pedido.getDataPedido());
+    
+                if (dataPedido.compareTo(inicio) >= 0 && dataPedido.compareTo(fim) <= 0) {
+                    pedidosEntreDatas.add(pedido);
+                }
             }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
         return pedidosEntreDatas;
     }
 
-    public ArrayList<Pedido> pedidoPorFuncionario(Funcionário funcionário) {
-        ArrayList<Pedido> pedidosPorFuncionario = new ArrayList<Pedido>();
+    public ArrayList<Pedido> pedidoPorFuncionario(Funcionario funcionario) {
+        ArrayList<Pedido> pedidosPorFuncionario = new ArrayList<>();
         for(Pedido pedido : pedidos) {
-            if(pedido.getFuncionario() == funcionário) {
+            if(pedido.getFuncionario() == funcionario) {
                 pedidosPorFuncionario.add(pedido);
             }
         }
         return pedidosPorFuncionario;
     }
 
-    public ArrayList<Pedido> pedidoPorDescricao(String descrição) {
-        ArrayList<Pedido> pedidosPorDescricao = new ArrayList<Pedido>();
+    public ArrayList<Pedido> pedidoPorDescricao(String descricao) {
+        ArrayList<Pedido> pedidosPorDescricao = new ArrayList<>();
         for(Pedido pedido : pedidos) {
-            ArrayList<Item> itens = pedido.getItens();
+            ArrayList<Item> itens = (ArrayList<Item>) pedido.getItens();
             for(Item item : itens) {
-                if(item.getDescricao() == descrição) {
+                if(item.getDescricao().equals(descricao)) {
                     pedidosPorDescricao.add(pedido);
                 }
             }
         }
         return pedidosPorDescricao;
     }
+
+    
 }
