9115c7efb1bb13aa1379e77ad615cb5a11d5866e
Carlos <cadu2001moraes@gmail.com>
2023-10-18 07:23:45 -0300

    Exlclusão isAdmin do Usuario

--- Usuario.java
+++ Usuario.java
@@ -3,13 +3,11 @@ public class Usuario {
 	private int identificador;
 	private String nome;
 	private String iniciais;
-	private boolean isAdmin;
 
-	public Usuario(int identificador, String nome, String iniciais, boolean isAdmin) {
+	public Usuario(int identificador, String nome, String iniciais) {
 		this.identificador = identificador;
 		this.nome = nome;
 		this.iniciais = iniciais;
-		this.isAdmin = isAdmin;
 	}
 
 	public int getIdentificador() {
@@ -23,9 +21,4 @@ public class Usuario {
 	public String getIniciais() {
 		return this.iniciais;
 	}
-
-	public boolean isAdmin() {
-		return this.isAdmin;
-	}
-
 }
